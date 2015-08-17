eaa01220d938d6e85b8761164d84acf742d03d7a
Iván Pondal <ivan.pondal@gmail.com>
2015-08-16 23:36:26 -0300

    Add gdb config

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ ln -r -i -s -t ~ bin .vim
 
 ln -r -i -s -t ~/.config i3 i3status
 
-ln -r -i -s -t ~ .gitconfig .vimrc .xinitrc .Xresources .bashrc .drirc
+ln -r -i -s -t ~ .gitconfig .vimrc .xinitrc .Xresources .bashrc .drirc .gdbinit
